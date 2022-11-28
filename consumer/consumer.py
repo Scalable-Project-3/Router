22b5eda91c6ec0d289b0af236cd6e2aec8fbe113
孔祥睿 <100473178+kongxr123@users.noreply.github.com>
2022-11-28 20:50:35 +0000

    consumer

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,39 +1,90 @@
 import socket
-import threading
-from multiprocessing import Process, Queue, JoinableQueue
 import time
 import random
+import sys
+import threading
+import asyncio
+from prompt_toolkit.patch_stdout import patch_stdout
+from prompt_toolkit.application import get_app
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit import prompt
+from prompt_toolkit.shortcuts import PromptSession
+import pandas as pd
+import csv
 
 ROUTER_PORT = 33333
 CONSUMER_PORT = 33533
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 class Consumer:
     def __init__(self, host):
         self.host = host
 
     def listen_device(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((self.host, CONSUMER_PORT))
-        while True:
-            msg, addr = sock.recvfrom(1024)
-            addr_str = addr[0] + ':' + str(addr[1])
-            print(msg)
+        try:
+            sock.bind((self.host, CONSUMER_PORT))
+            while True:
+                msg, addr = sock.recvfrom(1024)
+                print(msg)
 
-    def send_interest():
-        interest_name = ['Bob/Temperature', 'Alice/speed', 'Ben/pressure']
-        requester_name = ['diver/area/1/Bob', 'diver/area/2/Alice', 'diver/area/3/Ben']
-        for Interest_Name in interest_name:
-            for Requester_Name in requester_name:
-                message = f'interest,{Interest_Name},{Requester_Name}'.encode('utf-8')
-                sock.sendto(message, ("127.0.0.1", 34333))
+        finally:
+            sock.close()
+            print('socket close')
 
-    def main(self):
-        listen_thread = threading.Thread(target=consumer.listen_device())
-        listen_thread.start()
-        self.send_interest()
+    def test(self, interest_name):
+        sock.sendto(interest_name.encode(), ("127.0.0.1", 34333))
+
+    async def send_interest(self):
+        isLoop=True;
+        kb=KeyBindings()
+        @kb.add('c-c')
+        async def _(event):
+            nonlocal isLoop
+            isLoop=False
+            event.app.exit()
+
+        session = PromptSession()
+        while isLoop:
+            try:
+                interest_name = await session.prompt_async('>>', key_bindings=kb)
+                with open('./configure.csv', 'r') as f:
+                    reader = csv.reader(f)
+                    console=interest_name.split(',')[1].split('/')[0]+'/'+interest_name.split(',')[1].split('/')[1]+'/'+interest_name.split(',')[1].split('/')[2]
+                    for row in reader:
+                        if (row[0]== console):
+                            msg = bytes(interest_name, 'utf-8')
+                            print(str(row[1]))
+                            sock.sendto(msg, (str(row[1]), 34333))
+                print('send interest_name: %s' % interest_name)
+                t = threading.Thread(target = self.test, args = (interest_name,))
+                t.setDaemon(True)
+                t.start()
+            except KeyboardInterrupt:
+                return
+
+    async def main(self):
+        head_row = pd.read_csv('./configure.csv', nrows=0)
+        head_row_list = list(head_row)
+        csv_result = pd.read_csv('./configure.csv', usecols=head_row_list)
+        with patch_stdout():
+            try:
+                await self.send_interest()
+            finally:
+                pass
+
+    def run(self):
+        try:
+            from asyncio import run
+        except ImportError:
+            asyncio.run_until_complete(self.main())
+        else:
+            asyncio.run(self.main())
+
 
 if __name__ == '__main__':
     host = socket.gethostbyname(socket.gethostname())
     consumer = Consumer(host)
-    consumer.main()
+    listen_thread = threading.Thread(target=consumer.listen_device)
+    listen_thread.setDaemon(True)
+    listen_thread.start()
+    consumer.run()
+
